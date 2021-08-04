3c5e15b30830439a2c06c5fd6ea6fc27bd3eba78
5096064 <77618902+5096064@users.noreply.github.com>
2021-08-04 21:28:44 +0800

    Update xmSports3.js

--- xmSports3.js
+++ xmSports3.js
@@ -33,7 +33,7 @@ let dataJSON = "%5B%7B%22data_hr%22%3A%22%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F%5C%2F9L%
 const headers = {
   'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 }
-let login_token = 'NQVBQFJyQktGHlp6QkpbRI5LRl5qek4uXAQEBAAAAAMwYx2ahMObkmGUaB0Js68RO6gJlcuP82ObuKJIF19rn3So_GiHyPA6vNXYojQH02-YB7JKoH74_sBFpAxnpi_esm60zNNw9gyWjiJUooATbMaxdAbBVBr0uvc9CgD1TLrUfKpk3L-eH90R_3LjBrHbif7chsGDVz6flq9ff_VMilLuoG-cUdR586oSw8Oi_uTE35M2ctFdUVF7S2OYxNY';
+let login_token = 'NQVBQFJyQktGHlp6QkpbRl5LRl5qek4uXAQEBAAAAAFE_tBWWWcVsl8PUw25PVgcBSeVrr8x6tHINqWsTOMoU4qpCTTIKyw0S5qvrtQ6HGjmohLf7-89m3DlJb97cVnUXdtGBClOWlSu1HnMhis947-g8V-vpi4YN4H7yM-NCTPQxQaD8iYNyfs_5w8FAqUAUkSkq2af1RALX_IHGG-A43E2MiE3M7_vqP9NPW-UcnIDCj5yhWU8-WLfUafWrusw';
 //需要修改的运动步数波动范围，脚本默认修改步数范围为1w9到2w5
 const step = randomFriendPin($.getdata('xmMinStep')*1 || 20000, $.getdata('xmMaxStep')*1 || 30000);
 function getToken() {
